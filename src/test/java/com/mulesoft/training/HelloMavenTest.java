a69824eefdc2a015f7e94a98024498dbff568dcc
Yanelis Hernandez <yanehr@gmail.com>
2019-03-05 22:06:36 -0500

    triggering build

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -22,7 +22,7 @@ public class HelloMavenTest extends FunctionalTestCase {
     
     @Override
     protected String getConfigFile() {
-        return "maven-project.xml";
+        return "maven-project.xml"; 
     }
 
 }
